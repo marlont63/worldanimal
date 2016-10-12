f99c996dd2382f3786baeb951bf3c8a102ac2c59
Marlon Raschid Tavarez Parra <marlonraschidtavarezparra@MacBook-Pro-de-Marlon.local>
2016-10-12 17:36:33 +0200

    Initial Commit

---
+++ World animal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  World animal
+//
+//  Created by Marlon Raschid Tavarez Parra on 12/10/16.
+//  Copyright © 2016 MyBCloud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
